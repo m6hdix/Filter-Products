a202421249016255358f7e957a63245e2bf291b8
Mehdi Gholam Hosseini <96904286+m6hdix@users.noreply.github.com>
2022-06-08 17:52:27 +0430

    new feeature

--- assets/index.js
+++ assets/index.js
@@ -12,16 +12,51 @@ document.addEventListener("DOMContentLoaded", () => {
 
         })
         .catch(e => console.log(e))
+
+
 })
 
 function fllterApp(_product, _fillter) {
     const fillterResult = _product.filter((p) => {
-        return p.title.toLowerCase().includes(_fillter.searchItems.toLowerCase())
+        return p.class.toLowerCase().includes(_fillter.searchItems.toLowerCase())
+
     })
-    console.log(fillterResult);
+    var targetPirint = document.querySelector(".products")
+    targetPirint.innerHTML = ""
+    fillterResult.forEach((element) => {
+        const pirinter = document.createElement("div")
+        pirinter.classList.add("card-items")
+        pirinter.innerHTML = `   <figure><img src=${element.img} alt="Shoes" /></figure>
+        <div class="card-body">
+            <h2 class="card-title">${element.title}</h2>
+            <p>${element.price}</p>
+            <div class="card-actions ">
+                <button class="btn ">Buy Now</button>
+            </div>
+        </div>`
+        targetPirint.append(pirinter)
+
+
+    });
 }
 const searchBox = document.querySelector("#search")
 searchBox.addEventListener("input", (e) => {
     fillterTarget.searchItems = e.target.value
     fllterApp(allProdruct, fillterTarget);
+
+})
+
+const brns = document.querySelectorAll(".btn")
+brns.forEach((e) => {
+    e.addEventListener("click", (el) => {
+        const tareget = el.target.dataset.filter
+        fillterTarget.searchItems = tareget
+        fllterApp(allProdruct, fillterTarget);
+        if (tareget == "All") {
+            const fillterTarget = {
+                searchItems: ""
+            }
+            fllterApp(allProdruct, fillterTarget);
+        }
+    });
 })
